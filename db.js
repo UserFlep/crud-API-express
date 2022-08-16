3f06ed60478eb75fb7a36017263d7a5fdd5f47d8
Ivan <era.user.ivan@gmail.com>
2022-08-16 18:43:12 +0300

    webstorm

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg')
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
 })
 
 module.exports = pool;
